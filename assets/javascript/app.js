cdf53693e55e41523e42ad6d313676f73155f705
mmrydz <megrydzewski@gmail.com>
2019-03-26 11:05:33 -0400

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+/* global moment firebase */
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCUa3OmzBQAV9MHxQg6Pgl2s5533V5qjEI",
+    authDomain: "coder-bay-fee9d.firebaseapp.com",
+    databaseURL: "https://coder-bay-fee9d.firebaseio.com",
+    storageBucket: "coder-bay-fee9d.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  // Create a variable to reference the database
+  var database = firebase.database();
+  
+  // Initial Values
+
+  
+  // --------------------------------------------------------------
+  
+  // At the initial load and subsequent value changes, get a snapshot of the stored data.
+  // This function allows you to update your page in real-time when the firebase database changes.
+  database.ref().on("value", function(snapshot) {
+  }  
